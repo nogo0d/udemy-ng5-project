df7703f2abded888424e5a4efe7b1d9f7dc50dd2
nogo0d <jmonteiro205@gmail.com>
2018-02-20 12:12:32 +0000

    feat: add recipe book "placeholder" components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../model/recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -6,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+    recipes: Recipe[] = [
+        new Recipe(
+            'Francesinha',
+            'The most "TRIPEIRO" dish.',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLLC1iPp5Uv1a0zTpl82IsntGubr8LjUwlGloCirAjNDO771oZrw'
+        ),
+        new Recipe(
+            'Francesinha',
+            'The most "TRIPEIRO" dish.',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLLC1iPp5Uv1a0zTpl82IsntGubr8LjUwlGloCirAjNDO771oZrw'
+        ),
+        new Recipe(
+            'Francesinha',
+            'The most "TRIPEIRO" dish.',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLLC1iPp5Uv1a0zTpl82IsntGubr8LjUwlGloCirAjNDO771oZrw'
+        ),
+        new Recipe(
+            'Francesinha',
+            'The most "TRIPEIRO" dish.',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLLC1iPp5Uv1a0zTpl82IsntGubr8LjUwlGloCirAjNDO771oZrw'
+        )
+    ];
 
     constructor() { }
 
